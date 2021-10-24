7c1666675bc3d079b5da7b399bd4e920f98d7692
DESKTOP-F2RQ5U8\Ching <a22024125@gmail.com>
2021-10-24 18:59:34 +0800

    feat: 新增選項圖片

--- app/src/main/java/com/example/project4/Third.java
+++ app/src/main/java/com/example/project4/Third.java
@@ -1,9 +1,11 @@
 package com.example.project4;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,6 +15,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -25,13 +31,14 @@ public class Third extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         List<String> listStr = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             listStr.add(new String("第" + String.valueOf(i + 1) + "項"));
         }
         List = listStr;
         Log.d("qq", String.valueOf(List));
+
+
     }
 
 
@@ -48,10 +55,11 @@ public class Third extends Fragment {
         //mrecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),2));
         //mrecyclerView.setLayoutManager(new StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.VERTICAL));
 
-        ViewAdapter adapter = new ViewAdapter(List);
+        ViewAdapter adapter = new ViewAdapter(getActivity(), List);
 
         mrecyclerView.setAdapter(adapter);
 
         return v;
     }
+
 }
--- app/src/main/java/com/example/project4/ViewAdapter.java
+++ app/src/main/java/com/example/project4/ViewAdapter.java
@@ -1,5 +1,6 @@
 package com.example.project4;
 
+import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,20 +10,31 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 public class ViewAdapter extends RecyclerView.Adapter<ViewAdapter.ViewHolder> {
-
+    public Context context;
     private List<String> mListString;
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public TextView mTxt;
+        public ImageView mimage;
+
+
 
         public ViewHolder(View itemView) {
             super(itemView);
             mTxt = (TextView) itemView.findViewById(R.id.txt);
+            mimage = (ImageView) itemView.findViewById(R.id.image);
 
             itemView.setOnClickListener(this);
         }
@@ -34,7 +46,8 @@ public class ViewAdapter extends RecyclerView.Adapter<ViewAdapter.ViewHolder> {
         }
     }
 
-    public ViewAdapter(List<String> listString) {
+    public ViewAdapter(Context context, List<String> listString) {
+        this.context = context;
         mListString = listString;
         Log.d("qq", "mListString is " + String.valueOf(mListString));
     }
@@ -43,7 +56,6 @@ public class ViewAdapter extends RecyclerView.Adapter<ViewAdapter.ViewHolder> {
     @Override
     public ViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recycler_view, viewGroup, false);
-
         ViewHolder viewHolder = new ViewHolder(v);
         return viewHolder;
     }
@@ -51,7 +63,22 @@ public class ViewAdapter extends RecyclerView.Adapter<ViewAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ViewAdapter.ViewHolder viewHolder, int i) {
+
+        /*Glide.with(context)
+                .load("https://i2.kknews.cc/SIG=2srln3q/174s80001n227snr9n00r.jpg")
+                .placeholder(android.R.drawable.star_on)
+                .error(R.color.white)
+                .fitCenter()
+                .into(viewHolder.mimage);*/
+
+        Picasso.with(context)
+                .load("https://i2.kknews.cc/SIG=2srln3q/174s80001n227snr9n00r.jpg")
+                .centerCrop()
+                .resize(320, 320)
+                .into(viewHolder.mimage);
+
         viewHolder.mTxt.setText(mListString.get(i));
+
     }
 
     @Override
